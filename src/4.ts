b2ec39faddfc382a24d9bb16aea824550976922a
Salnikova-Anna <anna_val@ukr.net>
2023-11-02 11:55:13 +0200

    interfaces and classes

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {
+        this.key = key;
+    }
+
+    getKey() {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    private tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key) {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+    }
+    
+}
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
